22ce36f15475f1577648e8e9c12f4179df191f6c
guidzn1 <gui09lima@gmail.com>
2024-03-23 10:41:03 -0300

    Jogo finalizado

--- app/validacao.js
+++ app/validacao.js
@@ -12,13 +12,16 @@ function verificaValorValidoChute(chute){
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
         <h2>Você acertou!</h2>
-        <h3>O número secreto era ${numeroSecreto}</h3>`
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>`
+
     }else if (numero>numeroSecreto) {
-        elementoChute.innerHTML += `<div >O número secreto é menor<i class="fa-solid fa-down-up-long"></i></div>`
+        elementoChute.innerHTML += `<div >O número secreto é menor<i class="fa-solid fa-arrow-down-long"></i></div>`
         
     }else{
         elementoChute.innerHTML += `<div >O número secreto é maior<i class="fa-solid fa-arrow-up-long"></i></div>`
     }
+    
 }
 
 function chuteforInvalido(numero){
@@ -28,3 +31,10 @@ function chuteforInvalido(numero){
 function numeroMaiorOUMenorQueOValoe(numero){
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e =>{
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+        
+    }
+})
